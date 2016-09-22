7df69ecde0c6e990640286b2d6f9cb60595082a8
Cheng-Yu <e88885@gmail.com>
2016-09-22 14:35:03 +0800

    更新

--- src/study/SeekBarMenu/MainActivity.java
+++ src/study/SeekBarMenu/MainActivity.java
@@ -8,14 +8,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import study.SeekBarMenu.Adapter.SeekBarListener;
 
 public class MainActivity extends Activity implements OnNavigationListener {
 
+	TextView TextView1, TextView2, TextView3, TextView4;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		TextView1 = (TextView) findViewById(R.id.TextView1);
+		TextView2 = (TextView) findViewById(R.id.TextView2);
+		TextView3 = (TextView) findViewById(R.id.TextView3);
+		TextView4 = (TextView) findViewById(R.id.TextView4);
+		
 		ActionBar bar = getActionBar();
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("bar 0 ");
@@ -31,7 +40,20 @@ public class MainActivity extends Activity implements OnNavigationListener {
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser, int positionInList) {
 				Log.e("", "onProgressChanged " + progress + " position in list" + positionInList);
-
+				
+				if(positionInList == 0){
+					TextView1.setText("onProgressChanged " + progress + " position in list" + positionInList);
+				}
+				if(positionInList == 1){
+					TextView2.setText("onProgressChanged " + progress + " position in list" + positionInList);
+				}
+				if(positionInList == 2){
+					TextView3.setText("onProgressChanged " + progress + " position in list" + positionInList);
+				}
+				if(positionInList == 3){
+					TextView4.setText("onProgressChanged " + progress + " position in list" + positionInList);
+				}
+				
 			}
 
 			@Override
